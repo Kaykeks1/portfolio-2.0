4e15df54bada310e06039da72276878a2749ae91
kayode <kayuskeks@gmail.com>
2023-07-20 00:25:40 +0100

    fixed missing key warning

--- src/sections/projects/Projects.tsx
+++ src/sections/projects/Projects.tsx
@@ -12,14 +12,14 @@ export default function Projects({ id }: Props) {
       <h3 className='subtitle'>Things I've built so far</h3>
       <div className='project-list'>
         {
-          [1,2,3,4,5,6,7].map((_, k) => <div className='project-item' style={{ backgroundImage: `url(${backgroundImage})` }} key={k}>
-            <div className='project-item-overlay' key={k}>
+          [1,2,3,4,5,6,7].map((_, key) => <div className='project-item' style={{ backgroundImage: `url(${backgroundImage})` }} key={key}>
+            <div className='project-item-overlay'>
               <div>
                 <h3 className='project-title'>LMS</h3>
                 <p className='project-description'>Helps librarians to manage day to day activities</p>
                 <p className='project-stacks'>
                   {
-                    [1,2,3].map(i => <span>React</span>)
+                    [1,2,3].map((i, k) => <span key={k}>React</span>)
                   }
                 </p>
               </div>
