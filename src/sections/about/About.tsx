7c3069f5b5e58224bf4b0d98d1da5aa864b049ca
kayode <kayuskeks@gmail.com>
2023-07-21 22:07:23 +0100

    added data to about section

--- src/sections/about/About.tsx
+++ src/sections/about/About.tsx
@@ -1,3 +1,4 @@
+import { ReactComponent as OfficeIcon } from '../../images/svgs/office.svg';
 import "./About.css"
 
 type Props = {
@@ -6,16 +7,43 @@ type Props = {
 }
 
 export default function About({ id, onView }: Props) {
+  const experiences = [
+    {
+      period: 'Jul 2018 - Jul 2019',
+      position: 'Frontend developer Intern',
+      company: 'Fieldinsight',
+      summary: 'Gained knowledge about modern web development tools to create websites compatible with multiple browsers',
+    },
+    {
+      period: 'Feb 2020 - Dec 2021',
+      position: 'Fullstack Developer',
+      company: 'Chronos NG',
+      summary: 'Reverse engineered software for mobile operating systems with better user experience for a client in 3 months.',
+    },
+    {
+      period: 'Jan 2022 - Present',
+      position: 'Software Developer',
+      company: 'B54',
+      summary: 'Collaborated with project managers to select ambitious, but realistic coding milestones on pre-release software project development.',
+    },
+  ];
   return (
     <section className='container' id={id}>
       <h1 className='title'>About</h1>
+      <p className='subtext'>
+        I am a self-driven software developer seeking to work in a motivating environment to utilize my skills and to successfully develop applications with the aim of achieving corporate goals and organization objectives.
+      </p>
       <h3 className='subtitle'>Work experience</h3>
       <div className={`timeline ${onView && 'expanded'}`}>
         {
-          [1,2,3].map((i, k) => (<div className={`timeframe ${(k % 2) === 0  ? 'left' : 'right'} ${onView && 'opaque'}`} key={k}>
-            <p className="period">2017</p>
-            <p className="summary">Lorem ipsum dolor sit amet, quo ei simul congue exerci, ad nec admodum perfecto mnesarchum, vim ea mazim fierent detracto.</p>
-          </div>))
+          experiences.map((item, k) => (
+            <div className={`timeframe ${(k % 2) === 0  ? 'left' : 'right'} ${onView && 'opaque'}`} key={k}>
+              <p className="period">{item.period}</p>
+              <p className="position">{item.position}</p>
+              <p className="company">{item.company} <OfficeIcon className='office-icon'/></p>
+              <p className="summary">{item.summary}</p>
+            </div>
+          ))
         }
       </div>
     </section>
